a320d7f17849ec0ed4d826a5a4ef77db3e19216a
imhonglu <anonymouser.unknown@gmail.com>
2025-02-14 15:48:08 +0900

    feat(i18n): include default language in URL pathname

--- astro.config.js
+++ astro.config.js
@@ -6,9 +6,27 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://imhonglu.github.io",
+
   i18n: {
     locales: ["ko", "en"],
     defaultLocale: "en",
+    routing: {
+      defaultLocale: "en",
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false,
+    },
   },
-  integrations: [mdx(), sitemap()],
+
+  integrations: [
+    mdx(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          ko: "ko-KR",
+        },
+      },
+    }),
+  ],
 });
